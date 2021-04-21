7423a7ee3a7b4abb99facd827eca4ac16a4111d6
Claude Hangui <claude.hangui@gmail.com>
2021-04-21 21:30:03 +0000

    update jacoco version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,6 @@ plugins {
     id("com.android.application")
     `kotlin-android`
     id("core-binary-plugin")
-    jacoco
-    id("scriptplugins.jacoco-report")
-}
-
-jacoco {
-    toolVersion = "0.8.5"
+    id("maven-publish")
 }
 
